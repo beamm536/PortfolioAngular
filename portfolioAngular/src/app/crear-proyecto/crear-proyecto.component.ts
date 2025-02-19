7205bad6ce75867f1c29e6af2ba8be60a1d3aaa5
beamm536 <beamm023@gmail.com>
2025-02-19 20:51:26 +0100

    FUNCIONAL!
    > guarda en bd de firebase
    > mejorar estilos :) jeje

---
+++ portfolioAngular/src/app/crear-proyecto/crear-proyecto.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ServicioService } from '../servicio.service';
+
+@Component({
+  selector: 'app-crear-proyecto',
+  imports: [FormsModule],
+  templateUrl: './crear-proyecto.component.html',
+  styleUrl: './crear-proyecto.component.scss'
+})
+export class CrearProyectoComponent {
+
+  titulo: string = "";
+  descripcion: string = "";
+  tecnologias: string = "";
+  participantes: string = "";
+
+  constructor(private servicio:ServicioService){}
+
+  addProject(titulo: string, descripcion: string, tecnologias: string, participantes: string) {
+    this.servicio.crearProyecto(titulo, descripcion, tecnologias, participantes);
+  }
+}
