a55736b35968fb003996cf16b22cdf66778ee3ba
beamm536 <beamm023@gmail.com>
2025-02-20 10:02:27 +0100

    gitignore - claves firebase
    > para evitar problemas de seguridad

--- portfolioAngular/src/app/app.config.ts
+++ portfolioAngular/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { firebaseConfig } from '../environments/environment.firebase'; //indicamos dnd tenemos el archivo con las claves que van a ir al gitignore
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-  provideFirebaseApp(() => initializeApp({ projectId: "intermodular-angular-firebase", appId: "1:71119221853:web:1eb6609cdde9db033089b4", storageBucket: "intermodular-angular-firebase.firebasestorage.app", apiKey: "AIzaSyDVQ23AsZ2DqY3EooPM1f6CTzvyUY9l7-o", authDomain: "intermodular-angular-firebase.firebaseapp.com", messagingSenderId: "71119221853" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
 
 
