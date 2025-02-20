2ce48e4dc4281c5f0b0355cf44bda48db94270d6
beamm536 <beamm023@gmail.com>
2025-02-20 21:08:48 +0100

    creado el componente de update-proyect0s

---
+++ portfolioAngular/src/app/update-proyecto/update-proyecto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-proyecto',
+  imports: [],
+  templateUrl: './update-proyecto.component.html',
+  styleUrl: './update-proyecto.component.css'
+})
+export class UpdateProyectoComponent {
+
+}
