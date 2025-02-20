35831e2bd5fada0d1a5c29f81e2269c8c0020d92
beamm536 <beamm023@gmail.com>
2025-02-20 18:54:38 +0100

    navegacion basica con links, pero funcional
    > solo está entre la pagina para crear y la de listar
    > estilos del formulario de crear, pq estaba en formato scss

--- portfolioAngular/src/app/app.component.ts
+++ portfolioAngular/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CrearProyectoComponent } from './crear-proyecto/crear-proyecto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CrearProyectoComponent],
+  imports: [RouterOutlet, CrearProyectoComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
