3ee14f2ad31b7e35e05a9f73efbf3736a9b94c2a
Pedromrtz <pmartinezigual@gmail.com>
2025-02-24 08:30:45 +0100

    Creación del componente y lógica
    Proceso

--- portfolioAngular/src/app/listar-proyecto/listar-proyecto.component.ts
+++ portfolioAngular/src/app/listar-proyecto/listar-proyecto.component.ts
@@ -1,27 +1,36 @@
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { ServicioService } from '../servicio.service';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-proyecto',
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, CommonModule],
   templateUrl: './listar-proyecto.component.html',
   styleUrl: './listar-proyecto.component.css'
 })
 export class ListarProyectoComponent {
-
   proyectos: any;
+  proyectoSeleccionado: any = null;
+  mostrarModal = false;
 
-  constructor(private servicio:ServicioService){}
+  constructor(private servicio: ServicioService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.readProyectos();
   }
 
-  //la llamada a la base de datos - con el metodo GETPROYECTOS --> lo hacems desde Servicio
-  async readProyectos(){
+  async readProyectos() {
     this.proyectos = await this.servicio.getProyectos();
   }
 
+  abrirModal(proyecto: any) {
+    this.proyectoSeleccionado = proyecto;
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+    this.proyectoSeleccionado = null;
+  }
 }
