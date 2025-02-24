fbc78b1020fdf017c0232285878eb1d810c93be9
beamm536 <beamm023@gmail.com>
2025-02-24 10:59:45 +0100

    nuevo componente - registro-usuarios

---
+++ portfolioAngular/src/app/registro-user/registro-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-user',
+  imports: [],
+  templateUrl: './registro-user.component.html',
+  styleUrl: './registro-user.component.css'
+})
+export class RegistroUserComponent {
+
+}
