aeac22826331b02fea2f5ed7d415e5153b638278
beamm536 <beamm023@gmail.com>
2025-02-26 18:38:26 +0100

    creado el componente para el login
    con auth - firebase

--- portfolioAngular/src/app/app.routes.ts
+++ portfolioAngular/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { CrearProyectoComponent } from './crear-proyecto/crear-proyecto.componen
 import { ListarProyectoComponent } from './listar-proyecto/listar-proyecto.component';
 import { UpdateProyectoComponent } from './update-proyecto/update-proyecto.component';
 import { VerMasProyectoComponent } from './ver-mas-proyecto/ver-mas-proyecto.component';
+import { RegistroUserComponent } from './registro-user/registro-user.component';
 
 export const routes: Routes = [
 
     {path: 'crear-proyecto', component: CrearProyectoComponent},
     {path: 'listar-proyecto', component: ListarProyectoComponent},
     {path: 'actualizar-proyecto/:id', component: UpdateProyectoComponent },
-    { path: 'ver-mas-proyecto/:id', component: VerMasProyectoComponent }
+    { path: 'ver-mas-proyecto/:id', component: VerMasProyectoComponent },
+    { path: 'registro-user', component: RegistroUserComponent }
       
 
 ];
--- portfolioAngular/src/app/registro-user/registro-user.component.ts
+++ portfolioAngular/src/app/registro-user/registro-user.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro-user',
-  imports: [],
+  imports: [FormsModule], //he quitado de aqui el FormControl
   templateUrl: './registro-user.component.html',
   styleUrl: './registro-user.component.css'
 })
